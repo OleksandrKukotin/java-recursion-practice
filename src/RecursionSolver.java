fdb16b5b876cc0c39afeceb7d53f0efa1f62e5b1
Vladislav Hrushovyi <49609535+VladGrushevoi@users.noreply.github.com>
2023-05-25 17:19:31 +0300

    maybe is better

--- src/RecursionSolver.java
+++ src/RecursionSolver.java
@@ -3,15 +3,11 @@ import java.util.Arrays;
 public class RecursionSolver {
 
     private int factorialHelper(int number, int result) {
-        if (number == 1) {
-            return result;
-        }
-        result *= number;
-        return factorialHelper(number - 1, result);
+        return number * factorialHelper(number - 1, result);
     }
 
     public int factorial(int number) {
-        if (number == 0) {
+        if (number == 0 || number == 1) {
             return 1;
         }
         return factorialHelper(number, 1);
